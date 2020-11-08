78f186297dd03c4f0fbecce8dc1b541bb74e6e2f
Ty Schenk <tyschenk2@me.com>
2020-11-07 16:06:37 -0800

    spm support

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "CircleProgressView",
+    products: [
+        .library(
+            name: "CircleProgressView",
+            targets: ["CircleProgressView"]),
+    ],
+    targets: [
+        .target(
+            name: "CircleProgressView",
+            dependencies: [])
+    ]
+)
